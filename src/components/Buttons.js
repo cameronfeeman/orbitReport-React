4f009e89c1a862281340da779f2d8dce02cd4650
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-08-13 20:31:31 -0400

    fixed styling

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,18 +1,17 @@
-import  './styling.css';
+import './styling.css';
 import satData from "./satData";
 
-const Buttons = ({filterByType, setSat, displaySats}) => {
+const Buttons = ({ filterByType, setSat, displaySats }) => {
+
   return (
-    <div className= "flex-container">
-      {displaySats.map((sat, id) => {
-  return (
-      <button onClick={() => filterByType(sat)} key={id}>
-      {sat} Orbit
-      </button>
-  );
-      })}
+    <div className="flex-container">
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
       <button onClick={() => setSat(satData)}>All Orbits</button>
-      </div>
+    </div>
   );
 };
 
