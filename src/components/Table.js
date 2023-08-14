038a236e7d50ecc95fbb8bdfac98a4ae066aab99
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-08-13 20:35:26 -0400

    formatted doc

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,28 +1,28 @@
-import  './styling.css';
+import './styling.css';
 
 const Table = ({ sat }) => {
   return (
-      <table>
-       <thead>
+    <table>
+      <thead>
         <tr>
           <th>Name</th>
           <th>Type of Satellite</th>
           <th>Launch Date</th>
           <th>Status</th>
         </tr>
-        </thead>
-        <tbody>
-           {sat.map((data, id) => (
-        <tr key ={id}>
-          <td>{data.name}</td>
-          <td>{data.type}</td>
-          <td>{data.launchDate}</td>
-          <td>{data.operational ? "Active" : "Inactive"}</td>
+      </thead>
+      <tbody>
+        {sat.map((data, id) => (
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? "Active" : "Inactive"}</td>
           </tr>
         ))};
-       </tbody> 
-       
-      </table>
+      </tbody>
+
+    </table>
   )
 };
 
